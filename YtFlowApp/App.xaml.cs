50ad405127fc37b448bf01b2508ddea669377769
ProJend <782041066@qq.com>
2020-04-13 15:38:09 +0800

    Revert "Add some languages about: English, Simplified Chinese, Traditional Chinese."
    This reverts commit 2be3c2409d2cd5b235f4b6fa2c03bb13903f2f3c.

--- YtFlowApp/App.xaml.cs
+++ YtFlowApp/App.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.ExtendedExecution;
 using Windows.Storage;
-using Windows.UI;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -94,21 +91,7 @@ namespace YtFlow.App
                 }
                 // 确保当前窗口处于活动状态
                 Window.Current.Activate();
-
-                //入口
-                ExtendAcrylicIntoTitleBar();
             }
-        }
-
-        /// <summary>
-        /// 沉淀状态栏       
-        /// </summary>
-        private void ExtendAcrylicIntoTitleBar()
-        {
-            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
-            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
-            titleBar.ButtonBackgroundColor = Colors.Transparent;
-            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
         }
 
         /// <summary>
